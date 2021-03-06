6f24458609365c2f9989c7dd417b0ad140c033ab
Kane <kgin888@gmail.com>
2021-03-06 01:01:11 +0000

    resistor_color_duo.rb

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,7 +1,7 @@
 class ResistorColorDuo
   COLORS = %w[black brown red orange yellow green blue violet grey white]
   def self.value(resistor_colors)
-    resistor_colors[0..1].map{|x| COLORS.index(x)}.join().to_i
+    resistor_colors.first(2).map{|resistor_color| COLORS.index(resistor_color)}.join.to_i
   end
 end
 
